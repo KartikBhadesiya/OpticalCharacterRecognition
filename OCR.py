03e59dcd072e4614870a44ab4971da3610f7ad9d
Kartik Bhadesiya <kartik.bhadesiya@somaiya.edu>
2021-04-03 14:39:44 +0530

    V2

--- OCR.py
+++ OCR.py
@@ -1,39 +1,52 @@
 #importing the necessary packages
 import cv2
 import pytesseract
-pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
-img = cv2.imread("image1.png")
-gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+import numpy
+import imutils
 
-ret, threshold = cv2.threshold(gray,0,255,cv2.THRESH_OTSU | cv2.THRESH_BINARY_INV)
-rect_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (18, 18))
 
-dilation = cv2.dilate(threshold, rect_kernel, iterations = 1)
-contours, hierarchy = cv2.findContours(dilation, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+def OCR(img):
+    pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+    h, w, c = img.shape
+    #print(img.shape)
+    if w < 300:
+        img = imutils.resize(img, width=600)
+    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
-im2 = img.copy()
-file = open("recognized.txt", "w+")
-file.write("")
-file.close()
+    ret, threshold = cv2.threshold(gray,0,255,cv2.THRESH_OTSU | cv2.THRESH_BINARY_INV)
+    rect_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (18, 18))
 
-for cnt in contours:
-    x, y, w, h = cv2.boundingRect(cnt)
+    dilation = cv2.dilate(threshold, rect_kernel, iterations = 1)
+    contours, hierarchy = cv2.findContours(dilation, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
 
-    # Drawing a rectangle on copied image
-    rect = cv2.rectangle(im2, (x, y), (x + w, y + h), (0, 255, 0), 2)
+    im2 = img.copy()
+    file = open("recognized.txt", "w+")
+    file.write("")
+    file.close()
 
-    # Cropping the text block for giving input to OCR
-    cropped = im2[y:y + h, x:x + w]
+    for cnt in contours:
+        x, y, w, h = cv2.boundingRect(cnt)
 
-    # Open the file in append mode
-    file = open("recognized.txt", "a")
+        # Drawing a rectangle on copied image
+        rect = cv2.rectangle(im2, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
-    # Apply OCR on the cropped image
-    text = pytesseract.image_to_string(cropped)
+        # Cropping the text block for giving input to OCR
+        cropped = im2[y:y + h, x:x + w]
 
-    # Appending the text into file
-    file.write(text)
-    file.write("\n")
+        # Open the file in append mode
+        file = open("recognized.txt", "a")
 
-    # Close the file
-    file.close
+        # Apply OCR on the cropped image
+        text = pytesseract.image_to_string(cropped)
+
+        # Appending the text into file
+        file.write(text)
+        file.write("\n")
+
+        # Close the file
+        file.close
+
+img = cv2.imread('image3.png')
+# cv2.imshow("img",img)
+# cv2.waitKey(0)
+OCR(img)
